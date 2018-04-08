4c3e00af4ed962fc8414219611092c267529544a
Varun Amachi <varunamachi@gmail.com>
2018-04-08 21:50:06 +0530

    Support for any go+ng setup

--- build.sh
+++ build.sh
@@ -22,16 +22,43 @@ function recreateDir() {
     mkdir -p "$1"
 }
 
-#Path where code and required tools will be checked out
-rootPath="$1"
+#Path where code and required tools will be checked out. ex: /var/workspaces
+rootPath="${ROOT_PATH}"
+
+#Path where generated installer will be copied. ex: /mnt/builds/sprw/nightlies
+distPath="${DIST_PATH}"
+
+#Name of the installer that is going to be generated. 
+#ex: sprw_yyyymmdd_hhmmss.run
+distName="${DIST_NAME}"
+
+#Server command dir path relative to GOPATH ex: cmd/vaali
+srvCmdName="${SRV_CMD_NAME}"
+
+#Source directory of server peoject ex: github.com/varunamachi/vaali
+srvSrcGoPath="${SRV_SRC_GO_PATH}"
+
+wcName="${WEB_CLIENT_NAME}"
+
+wcRepo="${WEB_CLIENT_REPO}"
+
+wcProjectDir="${WEB_CLIENT_PROJECT_DIR}"
+
+#git repo of server project
+gitRepo="https://${srvSrcGoPath}"
+srvProjectDir="${GOPATH}/${srvSrcGoPath}"
+srvCmdDir="/cmd/${srvCmdName}"
+
+
 echo "Build root: ${rootPath}"
-
-#Path where generated installer will be copied
-distPath="$2"
 echo "Dist path: ${distPath}"
+echo "Server Command Name: ${srvCmdName}"
+echo "Git Repo:  ${gitRepo}"
+echo "Server Command Abs Path: ${srvCmdDir}"
+echo "Web Client Name: ${wcName}"
+echo "Web Client Repo: ${wcRepo}"
+echo "Web Client Project Dir: ${wcProjectDir}"
 
-#Name of the installer that is going to be generated
-distName="$3"
 
 createDir "${rootPath}"
 recreateDir "${distPath}"
@@ -53,36 +80,29 @@ fi
 makeSelfDir="${rootPath}/makeself"
 makeSelfExe="${makeSelfDir}/makeself.sh"
 PATH="${GOPATH}/bin":${PATH}
-vaaliDir="${GOPATH}/src/github.com/varunamachi/vaali"
-vaaliCmdDir="${vaaliDir}/cmd/vaali"
-sparrowDir="${rootPath}/sparrow"
 scriptName=$(readlink -f "$0")
 scriptDir=$(dirname "$scriptName")
 
-#get spw server source code
-# getLatestCode "https://github.com/varunamachi/spw"
-
-#get vaali source code - if spw is retrieved the dep should get this
-echo "Cloning Vaali to ${vaaliDir}..."
-getLatestCode "https://github.com/varunamachi/vaali" "${vaaliDir}" || exit -1
+#get server source code - if spw is retrieved the dep should get this
+echo "Cloning ${srvCmdName} to ${srvProjectDir}..."
+getLatestCode "${gitRepo}" "${srvProjectDir}" || exit -1
 echo "Done!"
 
-#go to vaali dir and install dependencies
-cd "${vaaliDir}" || exit -2
-echo "Entered ${vaaliDir}, Depping..."
+#go to project dir and install dependencies
+cd "${srvProjectDir}" || exit -2
+echo "Entered ${srvProjectDir}, Depping..."
 dep ensure || exit -2
 
-#build and install vaali executable
-cd "${vaaliCmdDir}" || exit -2
-echo "Entered ${vaaliCmdDir}, Installing..."
+#build and install server executable
+cd "${srvCmdDir}" || exit -2
+echo "Entered ${srvCmdDir}, Installing..."
 go install || exit -2
 
-#get sparrow source code
-echo "Clone Sparrow to ${sparrowDir}..."
-getLatestCode "https://github.com/varunamachi/sparrow" "${sparrowDir}" \
-    || exit -1
-echo "Done!, Entering ${sparrowDir}, Installing dependencies..."
-cd "${sparrowDir}" || exit -3
+#get web client source code
+echo "Clone ${wcName} to ${wcProjectDir}..."
+getLatestCode "${wcRepo}" "${wcProjectDir}" || exit -1
+echo "Done!, Entering ${wcName}, Installing dependencies..."
+cd "${wcProjectDir}" || exit -3
 npm install || exit -3
 echo "Performing build..."
 npm run build || exit -3
@@ -90,7 +110,7 @@ echo "Done!"
 
 
 #create a temporary directory for building the installer
-tempDir=$(mktemp -d -t "sparrow_xxxxxxxx") || exit -4
+tempDir=$(mktemp -d -t "${wcName}_xxxxxxxx") || exit -4
 mkdir "${tempDir}/static"
 echo "Created temp dir: ${tempDir}"
 
@@ -98,10 +118,10 @@ buildDate=$(date +"%d/%m/%Y %H:%M:%S")
 goVersion=$(go version)
 nodeVersion=$(node --version)
 npmVersion=$(npm --version)
-cd "$sparrowDir"
-hashSparrow=$(git log --format=%H -n 1)
-cd "$vaaliDir"
-hashVaali=$(git log --format=%M -n 1)
+cd "${wcProjectDir}"
+hashWC=$(git log --format=%H -n 1)
+cd "${srvProjectDir}"
+hashSrv=$(git log --format=%M -n 1)
 cd "${rootPath}"
 
 version_info="${tempDir}/version.json"
@@ -111,20 +131,20 @@ touch "${version_info}"
     echo "    nodeVersion: ${nodeVersion},"  
     echo "    npmVersion: ${npmVersion},"    
     echo "    goVersion: ${goVersion},"      
-    echo "    sparrowCommit: ${hashSparrow},"
-    echo "    vaaliCommit: ${hashVaali},"    
+    echo "    ${wcName}Commit: ${hashWC},"
+    echo "    ${srvCmdName}Commit: ${hashSrv},"    
     echo "    builtAt: ${buildDate}"         
     echo "}"                                 
 } >> "${version_info}"
 echo "Version file: "
 cat "${version_info}"
 #Copy stuff to dist directory
-echo "Copying static files ${sparrowDir}/dist/*"
-cp -R "${sparrowDir}/dist/*"    "${tempDir}/static" || exit -5
+echo "Copying static files ${wcProjectDir}/dist/*"
+cp -R "${wcProjectDir}/dist/*"   "${tempDir}/static" || exit -5
 echo "Copying ${scriptDir}/install.sh"
 cp "${scriptDir}/install.sh"    "${tempDir}"	    || exit -5
-echo "Copying ${GOPATH}/bin/vaali"
-cp "${GOPATH}/bin/vaali"        "${tempDir}"        || exit -5
+echo "Copying ${GOPATH}/bin/${srvCmdName}"
+cp "${GOPATH}/bin/${srvCmdName}" "${tempDir}"        || exit -5
 echo "Copying ${scriptDir}/run.sh"
 cp "${scriptDir}/run.sh"        "${tempDir}"        || exit -5
 
@@ -133,7 +153,7 @@ echo "Makeself: ${tempDir} --> ${distPath}"
 "${makeSelfExe}" --gzip --keep-umask \
     "${tempDir}"  \
     "${distPath}/${distName}" \
-    "Sparrow!"    \
+    "${wcName}!"    \
     "./install.sh"              || exit -5
 
 function cleanup() {
